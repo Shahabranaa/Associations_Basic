2085adcea8fd905a8d13c2fa95e46cb3403c37bb
Shahab Rana <shahabranaa@gmail.com>
2018-08-08 03:13:15 +0500

    added one to one

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_213825) do
+ActiveRecord::Schema.define(version: 2018_08_07_220657) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "street"
+    t.string "city"
+    t.string "country"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
