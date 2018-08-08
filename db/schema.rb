a13acfbfe3bb66eec687a2aaf009f5cab6eaeeb6
Shahab Rana <shahabranaa@gmail.com>
2018-08-08 07:10:05 +0500

    one to many through

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_015712) do
+ActiveRecord::Schema.define(version: 2018_08_08_020752) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "street"
@@ -65,12 +65,27 @@ ActiveRecord::Schema.define(version: 2018_08_08_015712) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "payment_histories", force: :cascade do |t|
+    t.integer "purse_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["purse_id"], name: "index_payment_histories_on_purse_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "purses", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "funds"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_purses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
